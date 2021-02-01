7153c98d50d186c9b9f48d9b4ffc44fd10271855
Gobind Puniani <gobindpuniani@gmail.com>
2021-01-31 23:15:00 -0800

    Finished first 3 problems

--- Lab-3.playground/Contents.swift
+++ Lab-3.playground/Contents.swift
@@ -1,3 +1,68 @@
 import Cocoa
 
-var str = "Hello, playground"
+// Problem 1
+
+var colorHex: [String:String] = [
+    "purple" : "#6C3483",
+    "blue" : "#2874A6",
+    "green": "#1E8449" ,
+    "orange" : "#D35400",
+    "yellow" : "#F1C40F",
+    "white" : "#FFFFFF",
+    "black" : "#000000"
+]
+
+// Iterate through each returned tuple and print each value
+for (_, hex) in colorHex {
+    print(hex)
+}
+
+
+// Problem 2
+
+var colors = ["red", "orange", "purple", "pink", "blue", "red", "green", "red", "blue", "purple", "pink" , "purple", "purple"]
+
+var colorDict: [String: Int] = [:]
+
+// Add new colors to the dictionary with color count of 1, otherwise increment the color's count by 1
+for color in colors {
+    if colorDict[color] == nil {
+        colorDict[color] = 1
+    }
+    else {
+        colorDict[color]! += 1
+    }
+}
+
+for (color, count) in colorDict {
+    print("\(color) - \(count)")
+}
+
+
+// Problem 3
+
+// Initialize the array with element 1, then add the sum previous element and the last element to the end of array
+func fibo(n: Int) -> [Int] {
+    var fiboNums: [Int] = [1]
+    var counter: Int = 1
+    var firstSummand = 1
+    while counter < n {
+        fiboNums += [firstSummand]
+        firstSummand += fiboNums[counter - 1]
+        counter += 1
+    }
+    return fiboNums
+}
+
+print(fibo(n: 7))
+print(fibo(n: 5))
+
+
+// Problem 4
+
+func powersOfTwo(N: Int) -> [Int] {
+    var exponent: Int = 0
+    var powers: [Int] = []
+    
+    return powers
+}
